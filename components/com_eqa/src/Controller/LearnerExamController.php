ad2c396f94dd5b802e6b2008d9324b48ad2f5f8b
Tuấn Anh <tuananh1982act@gmail.com>
2026-07-06 13:15:30 +0000

    Fixbug com_eqa: lỗi xử lý yêu cầu phúc khảo, đính chính

--- components/com_eqa/src/Controller/LearnerExamController.php
+++ components/com_eqa/src/Controller/LearnerExamController.php
@@ -7,50 +7,84 @@ use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\Controller\BaseController;
 use Joomla\CMS\Router\Route;
 use Kma\Component\Eqa\Administrator\Helper\GeneralHelper;
+use Kma\Component\Eqa\Site\Model\LearnerexamModel;
 
 /* The DEFAULT controller for the front end */
 class LearnerExamController extends BaseController
 {
 	public function RequestRegrading(): void
 	{
-		$app = Factory::getApplication();
-		$examId = $app->input->getInt('exam_id');
-		$learnerCode = GeneralHelper::getSignedInLearnerCode();
-
-		//Redirect in any case
-		$url = Route::_('index.php?option=com_eqa&view=learnerExams', false);
-		$this->setRedirect($url);
-
-		if(!is_integer($examId) || !is_string($learnerCode))
+		try
 		{
-			$this->setMessage('Yêu cầu thất bại', 'error');
-			return;
+			//Redirect in any case
+			$url = Route::_('index.php?option=com_eqa&view=learnerExams', false);
+			$this->setRedirect($url);
+
+			//Check token
+			$this->checkToken();
+
+			//A student account is required to continue
+			$learnerCode = GeneralHelper::getSignedInLearnerCode();
+			if($learnerCode === null)
+				throw new Exception('Tài khoản không hợp lệ');
+
+			//Get an array of exam IDs
+			$app = Factory::getApplication();
+			$examIds = $app->input->get('cid', null, 'array');
+			if(empty($examIds))
+				throw new Exception('Không xác định được môn thi');
+
+			/**
+			 * @var LearnerexamModel $model
+			 */
+			$model = $this->getModel();
+			foreach ($examIds as $examId)
+				$model->RequestRegrading($examId, $learnerCode);
 		}
-		$model = $this->getModel();
-		$model->RequestRegrading($examId, $learnerCode);
+		catch (Exception $e)
+		{
+			$this->setMessage($e->getMessage(), 'error');
+		}
 	}
 
 	public function RequestCorrection():void
 	{
-		$this->checkToken();
-		$app = Factory::getApplication();
-
-		$examId = $app->input->getInt('exam_id');
-		$markConstituent = $app->input->getInt('constituent');
-		$reason = $app->input->getString('reason');
-		$learnerCode = GeneralHelper::getSignedInLearnerCode();
-
-		//Redirect in any case
-		$url = Route::_('index.php?option=com_eqa&view=learnerExams', false);
-		$this->setRedirect($url);
-
-		if(!is_int($examId) || !is_int($markConstituent) || !is_string($reason) || !is_string($learnerCode))
+		try
 		{
-			$this->setMessage('Yêu cầu không hợp lệ', 'error');
-			return;
+			//Redirect in any case
+			$url = Route::_('index.php?option=com_eqa&view=learnerExams', false);
+			$this->setRedirect($url);
+
+			//Check token
+			$this->checkToken();
+
+			//A student account is required to continue
+			$learnerCode = GeneralHelper::getSignedInLearnerCode();
+			if($learnerCode === null)
+				throw new Exception('Tài khoản không hợp lệ');
+
+			//Get and validate
+			$app = Factory::getApplication();
+			$examId = $app->input->getInt('exam_id');
+			$markConstituent = $app->input->getInt('constituent');
+			$reason = $app->input->getString('reason');
+			if(!is_int($examId) || !is_int($markConstituent) || !is_string($reason))
+			{
+				$this->setMessage('Yêu cầu không hợp lệ', 'error');
+				return;
+			}
+
+			/**
+			 * Request a correction action
+			 * @var LearnerexamModel $model
+			 */
+			$model = $this->getModel();
+			$model->RequestCorrection($examId, $learnerCode, $markConstituent, $reason);
 		}
-		$model = $this->getModel();
-		$model->RequestCorrection($examId, $learnerCode, $markConstituent, $reason);
+		catch (Exception $e)
+		{
+			$this->setMessage($e->getMessage(), 'error');
+		}
 	}
 	public function ShowCorrectionRequestForm(): void
 	{
@@ -62,7 +96,7 @@ class LearnerExamController extends BaseController
 				throw new Exception('Không có môn thi nào được chỉ định');
 			$examId = $cid[0];
 
-			//Redirect in any case
+			//Redirect to the form
 			$url = Route::_('index.php?option=com_eqa&view=learnerExam&layout=requestCorrection&exam_id='. $examId, false);
 			$this->setRedirect($url);
 		}
